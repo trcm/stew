464f09bb7e26f10567dc8f565045b9fc413978e3
Tom <midson.trc@gmail.com>
2015-04-15 21:20:39 +1000

    added teh ability to view and edit stewdents after creation

--- spec.js
+++ spec.js
@@ -26,7 +26,7 @@ describe('angularjs homepage', function() {
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
     // element(by.model('stewdent.occupation')).sendKeys('Student');
     element(by.model('stewdent.phone_num')).sendKeys('Phone');
-    element(by.model('stewdent.student_email')).sendKeys('tom@stew.com');
+    element(by.model('stewdent.email')).sendKeys('tom@stew.com');
     // element(by.model('stewdent.address')).sendKeys('1 Queen St ');
     element(by.model('stewdent.city')).sendKeys('Brisbane');
     element(by.model('stewdent.state')).click();
@@ -34,12 +34,52 @@ describe('angularjs homepage', function() {
     element(by.model('stewdent.country')).sendKeys('Australia');
     
     element(by.model('skills.computer_based')).sendKeys('computer skillz');
-    element(by.model('skills.languages_coding')).sendKeys('coding');
+    element(by.model('skills.languages_coding')).sendKeys('C python bs');
     element(by.model('skills.personal')).sendKeys('personal');
     element(by.model('skills.software_skills')).sendKeys('languages');
     element(by.model('skills.languages_spoken')).sendKeys('languages');
-    element(by.model('skills.smartphone')).sendKeys('smartphone');
-    element(by.model('skills.tablet')).sendKeys('tablet');
+    // element(by.model('skills.smartphone')).sendKeys('smartphone');
+    // element(by.model('skills.tablet')).sendKeys('tablet');
+    
+    element(by.id('stewdentSubmit')).click();
+
+  });
+  
+  it('should create a long stewdent object 1', function() {
+    browser.get('/#/stewdents');
+    // browser.get('http://www.tommidson.com/#/stewdents');
+    // element(by.id('createButton')).click();
+    element(by.model('stewdent.first_name')).sendKeys('Long');
+    element(by.model('stewdent.last_name')).sendKeys('stew');
+    element(by.model('stewdent.gender')).click();
+    element(by.model('stewdent.dob')).click();
+    element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[1]/td[4]/a[@class='ui-state-default']")).click();
+    // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
+    element(by.model('stewdent.university')).sendKeys('UQ');
+    // element(by.model('stewdent.student_num')).sendKeys('1234567890');
+    element(by.model('stewdent.degree')).sendKeys('IT');
+    element(by.model('stewdent.start_year')).sendKeys("2013");
+    element(by.model('stewdent.end_year')).sendKeys("2015");
+    // element(by.model('stewdent.start_year')).click();
+    // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
+    // element(by.model('stewdent.end_year')).click();
+    // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
+    // element(by.model('stewdent.occupation')).sendKeys('Student');
+    element(by.model('stewdent.phone_num')).sendKeys('Phone');
+    element(by.model('stewdent.email')).sendKeys('long@stew.com');
+    // element(by.model('stewdent.address')).sendKeys('1 Queen St ');
+    element(by.model('stewdent.city')).sendKeys('Brisbane');
+    element(by.model('stewdent.state')).click();
+    element(by.model('stewdent.post_code')).sendKeys('4000');
+    element(by.model('stewdent.country')).sendKeys('Australia');
+    
+    element(by.model('skills.computer_based')).sendKeys("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque molestie convallis erat. Suspendisse sed mauris quis neque posuere congue consequat in turpis. Phasellus iaculis tortor at ex semper viverra. Quisque rutrum urna ut pulvinar porttitor. Curabitur dapibus porta aliquam. Duis ac odio imperdiet, tincidunt leo eu, dictum tellus. Phasellus ac eros rutrum leo pharetra ultricies sit amet vel ipsum. Sed erat mi, faucibus sed nisl at, tristique tincidunt sapien. Maecenas et arcu et erat dignissim molestie. Quisque tincidunt turpis at efficitur tincidunt. Donec sit amet lacus eget sem tincidunt commodo. Donec ac lacus eu neque interdum luctus sed at purus. In dignissim tellus nec enim ornare, sed hendrerit lorem blandit. Praesent urna velit, pulvinar id velit nec, molestie luctus nulla. Pellentesque et mollis diam. Maecenas non metus turpis.Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Suspendisse molestie lectus ac eros vestibulum placerat. Cras eleifend quis sem a cursus. Nullam pretium facilisis elementum. Donec tellus massa, ultrices nec feugiat vel, tempus eu ex. Proin porta, urna laoreet tincidunt iaculis, odio urna porttitor lectus, ac vestibulum enim lorem vel nulla. Donec a mollis mec tempus laoreet interdum. Nam luctus et justo at porta. Ut dictum mi in ante dignissim dapibus. Aenean pretium felis quis ante scelerisque pulvinar. Aenean lacinia lectus eu lorem euismod, ac tempus libero placerat. Curabitur iaculis ex ut magna sagittis, cursus commodo nibh varius. Donec malesuada mi id erat venenatis, ac consectetur dolor maximus. Quisque sollicitudin vitae nunc nec rhoncus. Ut condimentum ex nisi, ut sollicitudin quam pellentesque eu.Duis et fringilla nulla, eget fermentum nisl. Cras bibendum ligula a justo malesuada, eget tempus sem ultrices. Suspendisse eget lorem non nunc sagittis consectetur. Cras lorem leo, tincidunt a libero nec, consectetur fermentum quam. Nunc vehicula lacus efficitur augue pellentesque rutrum. Donec eu libero vitae leo vehicula efficitur. Aliquam dignissim odio eu interdum maximus. In dui felis, pellentesque quis metus sed, euismod pellentesque nulla. Cras porta est et euismod ");
+    element(by.model('skills.languages_coding')).sendKeys('programming languaes');
+    element(by.model('skills.personal')).sendKeys('personaLorem ipsum dolor sit amet, consectnas non metus turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Suspendisse molestie lectus ac eros vestibulum placerat. Cras eleifend quis sem a cursus. Nullam pretium facilisis elementum. Donec tellus massa, ultrices nec feugiat vel, tempus eu ex. Proin porta, urna laoreet tincidunt iaculis, odio urna porttitor lectus, ac vestibulum enim lorem vel nulla. Donec a mollis metus, facilisis lobortis lorem. Integer faucibus pulvinar urna, eget pharetra velit laoreet eget. Nunc tempus laoreet interdum. Nam luctus et justo at porta. Ut dictum mi in ante dignissim dapibus. Aenean pretium felis quis ante scelerisque pulvinar. Aenean lacinia lectus eu lorem euismod, ac tempus libero placerat. Curabitur iaculis ex ut magna sagittis, cursus commodo nibh varius. Donec malesuada mi id erat venenatis, ac consectetur dolor maximus. Quisque sollicitudin vitae nunc nec rhoncus. Ut condimentum ex nisi, ut sollicitudin quam pellentesque eu. Duis et fringilla nulla, eget fermentum nisl. Cras bibendum ligula a justo malesuada, eget tempus sem ultrices. Suspendisse eget lorem non nunc sagittis consectetur. Cras lorem leo, tincidunt a libero nec, consectetur fermentum quam. Nunctesque nulla. Cras porta est et euismod ultrices. In nec enim ex. Vivamus malesuada ante eget leo fringilla, sit amet luctus enim auctor. Phasellus sagittis consequat maximus. Integer sed quam quam. Aenean quis nulla luctus, condimentum neque non, malesuada erat. Sed est enim, placerat et sem ac, feugiat accumsan dolor. Cras faucibus convallis orci in pretium. Sid egestas nisl tincidunt quis. Donec ex nisi, cursus ut magna vel, volutpat ultricies justo. Praesent pellentesque sed diam tempor sollicitudin. Vestibulum faucibus faucibus sapien, vitae luctus risus accumsan dapibus. Mauris condimentum elit a metus maximus, facilisis sagittis ipsum malesuada. Suspendisse mattis egestas leo sed feugiat. Proin at iaculis ipsum. Donec commodo facilisis magna sed sollicitudin. Donec porta ipsum purus, ut consectetur nibh vestibulum at. Nullam tempor vitae nibh ac bibendum. Fusce tempus metus urna, et pharetra augue consectetur a. Fusce et mauris urna. Nunc finibus quam vitae nibh dapibus, quis egestas nunc consectetur. Pellentesque id.l');
+    element(by.model('skills.software_skills')).sendKeys('languagesLorem ipsum dolor sit ametlandit. Praesent urna velit, pulvinar id velit nec, molestie luctus nulla. Pellentesque et mollis diam. Maecenas non metus turpis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam erat volutpat. Suspendisse molestie lectus ac eros vestibulum placerat. Cras eleifend quis sem a cursus. Nullam pretium facilisis elementum. Donec tellus massa, ultrices nec feugiat vel, tempus eu ex. Proin porta, urna laoreet tincidunt iaculis, odio urna porttitor lectus, ac vestibulum enim lorem vel nulla. Donec a mollis metus, facilisis lobortis lorem. Integer faucibus pulvinar urna, eget pharetra velit laoreet eget. Nunc tempus laoreet interdum. Nam luctus et justo at porta. Ut dictum mi in ante dignissim dapibus. Aenean pretium felis quis ante scelerisque pulvinar. Aenean lacinia lectus eu lorem euismod, ac tempus libero placerat. Curabitur iaculis ex ut magna sagittis, cursus commodo nibh varius. Donec malesuada mi id erat venenatis, ac consectetur dolor maximus. Quisque sollicitudin vitae nunc nec rhoncus. Ut condimentum ex nisi, ut sollicitudin quam pellentesque eu. Duis et fringilla nulla, eget fermentum nisl. Cras bibendum ligula a justo malesuada, eget tempus sem ultrices. Suspendisse eget icitur. Aliquam dignissim odio eu interdum maximus. In dui felis, pellentesque quis metus sed, euismod pellentesque nulla. Cras porta est et euismod ultrices. In nec enim ex. Vivamus malesuada ante eget leo fringilla, sit amet luctus enim auctor. Phasellus sagittis consequat maximus. Integer sed quam quam. Aenean quis nulla luctus, condimentum neque non, malesuaretium. Sed vulputate enim a mauris pharetra, condimentum dignissim nibh ornare. Sed rhoncus vulputate orci, id egestas nisl tincidunt quis. Donec ex nisi, cursus ut magna vel, volutpat ultricies justo. Praesent pellentesque sed diam tempor sollicitudin. Vestibulum faucibus faucibus sapien, vitae luctus risus accumsan dapibus. Mauris condimentum elit a metus maximus, facilisis sagittis ipsum malesuada. Suspendisse mattis egestas leo sed feugiat. Proin at iaculis ipsum. Donec commodo facilisis magna sed sollicitudin. Donec porta ipsum purus, ut consectetur nibh vestibulum at. Nullam tempor vitae nibh ac bibendum. Fusce tempus metus urna, et pharetra augue consectetur a. Fusce et mauris urna. Nunc finibus quam vitae nibh dapibus, quis egestas nunc consectetur. Pellentesque id.');
+    element(by.model('skills.languages_spoken')).sendKeys('languages');
+    // element(by.model('skills.smartphone')).sendKeys('smartphone');
+    // element(by.model('skills.tablet')).sendKeys('tablet');
     
     element(by.id('stewdentSubmit')).click();
 
@@ -66,7 +106,7 @@ describe('angularjs homepage', function() {
     // element(by.model('stewdent.occupation')).sendKeys('occupation');
     element(by.model('stewdent.phone_num')).sendKeys('phone num');
     // element(by.model('stewdent.student_num')).sendKeys('sutdent num');
-    element(by.model('stewdent.student_email')).sendKeys('sam@stew.com');
+    element(by.model('stewdent.email')).sendKeys('sam@stew.com');
     // element(by.model('stewdent.address')).sendKeys('address');
     element(by.model('stewdent.city')).sendKeys('city');
     element(by.model('stewdent.state')).click();
@@ -78,8 +118,8 @@ describe('angularjs homepage', function() {
     element(by.model('skills.personal')).sendKeys('personal');
     element(by.model('skills.software_skills')).sendKeys('languages');
     element(by.model('skills.languages_spoken')).sendKeys('languages');
-    element(by.model('skills.smartphone')).sendKeys('smartphone');
-    element(by.model('skills.tablet')).sendKeys('tablet');
+    // element(by.model('skills.smartphone')).sendKeys('smartphone');
+    // element(by.model('skills.tablet')).sendKeys('tablet');
 
     // element(by.model('work.industry_one')).sendKeys('computer skillz');
     // element(by.model('work.industry_two')).sendKeys('coding');
@@ -111,7 +151,7 @@ describe('angularjs homepage', function() {
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[1]/td[4]/a[@class='ui-state-default']")).click();
     // element(by.model('stewdent.occupation')).sendKeys('occupation');
     element(by.model('stewdent.phone_num')).sendKeys('phone num');
-    element(by.model('stewdent.student_email')).sendKeys('iasdfsaftest.com');
+    element(by.model('stewdent.email')).sendKeys('iasdfsaftest.com');
     // element(by.model('stewdent.address')).sendKeys('address');
     element(by.model('stewdent.city')).sendKeys('city');
     element(by.model('stewdent.state')).click();
@@ -123,8 +163,8 @@ describe('angularjs homepage', function() {
     element(by.model('skills.personal')).sendKeys('personal');
     element(by.model('skills.software_skills')).sendKeys('languages');
     element(by.model('skills.languages_spoken')).sendKeys('languages');
-    element(by.model('skills.smartphone')).sendKeys('smartphone');
-    element(by.model('skills.tablet')).sendKeys('tablet');
+    // element(by.model('skills.smartphone')).sendKeys('smartphone');
+    // element(by.model('skills.tablet')).sendKeys('tablet');
 
     // element(by.model('work.industry_one')).sendKeys('computer skillz');
     // element(by.model('work.industry_two')).sendKeys('coding');
@@ -170,7 +210,7 @@ describe('angularjs homepage', function() {
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
     // element(by.model('stewdent.end_year')).click();
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
-    element(by.model('stewdent.student_email')).sendKeys('test@required.com');
+    element(by.model('stewdent.email')).sendKeys('test@required.com');
     element(by.model('stewdent.city')).sendKeys('city');
     element(by.model('stewdent.state')).click();
     element(by.model('stewdent.country')).sendKeys('country');
@@ -198,7 +238,7 @@ describe('angularjs homepage', function() {
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
     // element(by.model('stewdent.end_year')).click();
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
-    element(by.model('stewdent.student_email')).sendKeys('test@required.com');
+    element(by.model('stewdent.email')).sendKeys('test@required.com');
     element(by.model('stewdent.city')).sendKeys('city');
     element(by.model('stewdent.state')).click();
     element(by.model('stewdent.country')).sendKeys('country');
--- stew/static/js/admin/admin-controller.js
+++ stew/static/js/admin/admin-controller.js
@@ -2,8 +2,8 @@
 
 angular.module('stew')
   .controller('AdminController',
-	      ['$scope', '$http', '$location', 'lodash', 'Stewdent', 'stewdents', 'skills', 'token',
-	       function($scope, $http, $location, lodash, Stewdent, stewdents, skills, token) {
+	      ['$scope', '$http', '$location', '$modal', 'lodash', 'Stewdent', 'Skill', 'stewdents', 'skills', 'token',
+	       function($scope, $http, $location, $modal, lodash, Stewdent, Skill, stewdents, skills, token) {
 		 $scope.test = "test";
 		 // $scope.stewdents = stewdents.data;
 		 $scope.user = token.getUser();
@@ -16,30 +16,129 @@ angular.module('stew')
 		 $scope.stewdentsSfc = stewdents.data;
 		 $scope.skills = skills.data;
 
+		 $scope.combined = [];
+		 
 		 $scope.zipped = lodash.zip($scope.stewdents, $scope.skills);
 		 console.log($scope.zipped);
-		 // console.log($scope.zipped);
-		 $scope.update = function (id) {
-		   $scope.stewdent = Stewdent.get({id: id});
-		   $scope.open(id);
-		 };
-
+		 lodash.forEach($scope.zipped, function(i) {
+		   $scope.combined.push(lodash.merge(i[0], i[1]));
+		 });
+		 console.log($scope.combined);
+		 
+		 $scope.combinedSf = $scope.combined;
 		 // console.log(token.getUser());
 		 // if (token.authenticated) {
 		 //   console.log('auth');
 		 // }
 		 
 		 $scope.delete = function (id) {
-		   // console.log(id);
+		   console.log(id.pk);
 		   Stewdent.delete({id: id.pk}, function() {
 		     $scope.stewdents = Stewdent.query();
 		   });
 		 };
 
-		 $scope.logout = function() {
-		   // console.log("logout");
-		   token.logout();
-		   $location.path('/stewdents');
+		 // $scope.logout = function() {
+		 //   // console.log("logout");
+		 //   token.logout();
+		 //   $location.path('/stewdents');
+		 // };
+
+		 $scope.edit = function(stewdent) {
+		   var id = stewdent.stewdent;
+		   var stewdentEdit = lodash.find($scope.stewdents, function(s) {
+		     return s.stewdent == id;
+		   });
+		   var skillEdit = lodash.find($scope.skills, function(s) {
+		     return s.stewdent == id;
+		   });
+
+		   var stewEdit = $modal.open({
+		     templateUrl: 'static/js/views/admin/stewdentEdit-modal.html',
+		     controller: 'StewdentEditController',
+		     size: 'lg',
+		     resolve: {
+		       stewdent: function() {
+			 return stewdentEdit;
+		       },
+		       skill: function() {
+			 return skillEdit;
+		       }
+		     }
+		   });
+
+		   stewEdit.result.then(function(data) {
+		     delete data[0].skills;
+		     delete data[0].tablet;
+		     delete data[0].smartphone;
+		     delete data[0].languages_spoken;
+		     delete data[0].languages_coding;
+		     delete data[0].personal;
+		     delete data[0].computer_based;
+		     delete data[0].software_skills;
+		     delete data[0].content;
+		     console.log(data[1]);
+		     var skillData = data[1];
+		     // save the new data using put methods
+		     $http.put('/stewdent/' + id, data[0]).
+		       success(function(data) {
+			 $http.put('/skill/' + id, skillData).
+			   success(function(data) {
+			       // update the combined 
+			     var dents = Stewdent.query();
+			     var skill = Skill.query();
+			     $scope.zipped = lodash.zip(dents, skill);
+			     console.log($scope.zipped);
+			     lodash.forEach($scope.zipped, function(i) {
+			       $scope.combined.push(lodash.merge(i[0], i[1]));
+			     });
+			     console.log($scope.combined);
+			     
+			     $scope.combinedSf = $scope.combined;
+			   })
+			   .error(function(data) {
+			     alert("Oooops, something is broken.  We're still in beta, please contact stewhelp@gmail.com");
+			     console.log(data);
+			   });
+		       })
+		       .error(function(data) {
+			 alert("Oooops, something is broken.  We're still in beta, please contact stewhelp@gmail.com");
+			 console.log(data);
+		       });
+		   });
 		 };
 		 
-	       }]);
+
+		 $scope.view = function(stewdent) {
+		   console.log(stewdent);
+		   $modal.open({
+		     templateUrl: 'static/js/views/admin/stewdentinfo-modal.html',
+		     controller: 'StewdentInfoController',
+		     size: "lg",
+		     resolve: {
+		       stewdent: function() {
+			 return stewdent;
+		       }
+		     }
+		   });
+		 };
+
+	       }])
+  .controller('StewdentInfoController', ['$scope', '$modalInstance', 'stewdent', function($scope, $modalInstance, stewdent) {
+    $scope.stewdent = stewdent;
+    $scope.close = function() {
+      $modalInstance.close('close');
+    };
+  }])
+  .controller('StewdentEditController', ['$scope', '$modalInstance', 'stewdent', 'skill', function($scope, $modalInstance, stewdent, skill) {
+    $scope.stewdent = stewdent;
+    $scope.skills = skill;
+    
+    $scope.genders = ['Male', 'Female', 'Not specified'];
+    $scope.states = ['QLD', 'NSW', 'VIC', 'TAS', 'SA', 'WA', 'NT', 'ACT'];
+
+    $scope.ok = function() {
+      $modalInstance.close([$scope.stewdent, $scope.skills]);
+    };
+    
+  }]);
