e23e8514272b0686dc13614741aa1421a4492233
Tom <midson.trc@gmail.com>
2015-04-18 14:30:10 +1000

    added the alphanumeric test to spec.js

--- conf.js
+++ conf.js
@@ -12,7 +12,6 @@ exports.config = {
   // The timeout for each script run on the browser. This should be longer
   // than the maximum time your application needs to stabilize between tasks.
   allScriptsTimeout: 110000,
-
   // A base URL for your application under test. Calls to protractor.get()
   // with relative paths will be prepended with this.
   baseUrl: 'http://localhost:' + (process.env.PORT || '8000'),
@@ -51,6 +50,6 @@ exports.config = {
   //
   // See the full list at https://github.com/juliemr/minijasminenode
   jasmineNodeOpts: {
-    defaultTimeoutInterval: 30000
+    defaultTimeoutInterval: 60000
   }
 };
--- spec.js
+++ spec.js
@@ -1,7 +1,7 @@
 describe('angularjs homepage', function() {
+  
   it('should have a title', function() {
     browser.get('/#/stewdents');
-
     expect(browser.getTitle()).toEqual('Stew');
   });
 
@@ -25,7 +25,7 @@ describe('angularjs homepage', function() {
     // element(by.model('stewdent.end_year')).click();
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
     // element(by.model('stewdent.occupation')).sendKeys('Student');
-    element(by.model('stewdent.phone_num')).sendKeys('Phone');
+    element(by.model('stewdent.phone_num')).sendKeys('1234');
     element(by.model('stewdent.email')).sendKeys('tom@stew.com');
     // element(by.model('stewdent.address')).sendKeys('1 Queen St ');
     element(by.model('stewdent.city')).sendKeys('Brisbane');
@@ -40,7 +40,7 @@ describe('angularjs homepage', function() {
     element(by.model('skills.languages_spoken')).sendKeys('languages');
     // element(by.model('skills.smartphone')).sendKeys('smartphone');
     // element(by.model('skills.tablet')).sendKeys('tablet');
-    
+    element(by.model('agreeCheck')).click();
     element(by.id('stewdentSubmit')).click();
 
   });
@@ -65,7 +65,7 @@ describe('angularjs homepage', function() {
     // element(by.model('stewdent.end_year')).click();
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[2]/td[3]/a[@class='ui-state-default']")).click();
     // element(by.model('stewdent.occupation')).sendKeys('Student');
-    element(by.model('stewdent.phone_num')).sendKeys('Phone');
+    element(by.model('stewdent.phone_num')).sendKeys('1234');
     element(by.model('stewdent.email')).sendKeys('long@stew.com');
     // element(by.model('stewdent.address')).sendKeys('1 Queen St ');
     element(by.model('stewdent.city')).sendKeys('Brisbane');
@@ -81,6 +81,7 @@ describe('angularjs homepage', function() {
     // element(by.model('skills.smartphone')).sendKeys('smartphone');
     // element(by.model('skills.tablet')).sendKeys('tablet');
     
+    element(by.model('agreeCheck')).click();
     element(by.id('stewdentSubmit')).click();
 
   });
@@ -104,7 +105,7 @@ describe('angularjs homepage', function() {
     // element(by.model('stewdent.end_year')).click();
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[1]/td[4]/a[@class='ui-state-default']")).click();
     // element(by.model('stewdent.occupation')).sendKeys('occupation');
-    element(by.model('stewdent.phone_num')).sendKeys('phone num');
+    element(by.model('stewdent.phone_num')).sendKeys('4321');
     // element(by.model('stewdent.student_num')).sendKeys('sutdent num');
     element(by.model('stewdent.email')).sendKeys('sam@stew.com');
     // element(by.model('stewdent.address')).sendKeys('address');
@@ -126,7 +127,8 @@ describe('angularjs homepage', function() {
     // element(by.model('work.company_one')).sendKeys('personal');
     // element(by.model('work.company_two')).sendKeys('languages');
     // element(by.model('work.other_goals')).sendKeys('languages');
-
+    
+    element(by.model('agreeCheck')).click();
     element(by.id('stewdentSubmit')).click();
   });
 
@@ -150,7 +152,7 @@ describe('angularjs homepage', function() {
     // element(by.model('stewdent.end_year')).click();
     // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[1]/td[4]/a[@class='ui-state-default']")).click();
     // element(by.model('stewdent.occupation')).sendKeys('occupation');
-    element(by.model('stewdent.phone_num')).sendKeys('phone num');
+    element(by.model('stewdent.phone_num')).sendKeys('1234');
     element(by.model('stewdent.email')).sendKeys('iasdfsaftest.com');
     // element(by.model('stewdent.address')).sendKeys('address');
     element(by.model('stewdent.city')).sendKeys('city');
@@ -171,7 +173,8 @@ describe('angularjs homepage', function() {
     // element(by.model('work.company_one')).sendKeys('personal');
     // element(by.model('work.company_two')).sendKeys('languages');
     // element(by.model('work.other_goals')).sendKeys('languages');
-
+    
+    element(by.model('agreeCheck')).click();
     var clickable = false;
     var submit = element(by.id('stewdentSubmit'));
     expect(submit.isEnabled()).toBe(false);
@@ -192,7 +195,53 @@ describe('angularjs homepage', function() {
 
     
   });
+  
+  it("should cause the form to be invalid with alphanumeric cheracters", function() {
+    browser.get('/#/stewdents');
+    // element(by.id('createButton')).click();
+    element(by.model('stewdent.first_name')).sendKeys('ƒ∂ƒ∂ß˚¬∆');
+    element(by.model('stewdent.last_name')).sendKeys('tom');
+    element(by.model('stewdent.gender')).click();
+    element(by.model('stewdent.dob')).click();
+    element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[1]/td[4]/a[@class='ui-state-default']")).click();
+    element(by.model('stewdent.university')).sendKeys('university');
+    // element(by.model('stewdent.student_num')).sendKeys('sutdent num');
+    element(by.model('stewdent.degree')).sendKeys('degree');
+    element(by.model('stewdent.start_year')).sendKeys("2013");
+    element(by.model('stewdent.end_year')).sendKeys("2015");
+    // element(by.model('stewdent.start_year')).click();
+    // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[1]/td[4]/a[@class='ui-state-default']")).click();
+    // element(by.model('stewdent.end_year')).click();
+    // element(by.xpath("//div[@id='ui-datepicker-div']/table[@class='ui-datepicker-calendar']/tbody/tr[1]/td[4]/a[@class='ui-state-default']")).click();
+    // element(by.model('stewdent.occupation')).sendKeys('occupation');
+    element(by.model('stewdent.phone_num')).sendKeys('1234');
+    element(by.model('stewdent.email')).sendKeys('iasdfsaftest.com');
+    // element(by.model('stewdent.address')).sendKeys('address');
+    element(by.model('stewdent.city')).sendKeys('city');
+    element(by.model('stewdent.state')).click();
+    element(by.model('stewdent.post_code')).sendKeys('city');
+    element(by.model('stewdent.country')).sendKeys('country');
+    
+    element(by.model('skills.computer_based')).sendKeys('computer skillz');
+    element(by.model('skills.languages_coding')).sendKeys('coding');
+    element(by.model('skills.personal')).sendKeys('personal');
+    element(by.model('skills.software_skills')).sendKeys('languages');
+    element(by.model('skills.languages_spoken')).sendKeys('languages');
+    // element(by.model('skills.smartphone')).sendKeys('smartphone');
+    // element(by.model('skills.tablet')).sendKeys('tablet');
 
+    // element(by.model('work.industry_one')).sendKeys('computer skillz');
+    // element(by.model('work.industry_two')).sendKeys('coding');
+    // element(by.model('work.company_one')).sendKeys('personal');
+    // element(by.model('work.company_two')).sendKeys('languages');
+    // element(by.model('work.other_goals')).sendKeys('languages');
+    
+    element(by.model('agreeCheck')).click();
+    var clickable = false;
+    var submit = element(by.id('stewdentSubmit'));
+    expect(submit.isEnabled()).toBe(false);
+  });
+  
   it("Should only need the required fields to create the objects", function() {
     // browser.get('http://www.tommidson.com/#/stewdents');
     browser.get('/#/stewdents');
@@ -218,6 +267,7 @@ describe('angularjs homepage', function() {
     element(by.model('skills.computer_based')).sendKeys('computer skillz');
     element(by.model('skills.software_skills')).sendKeys('languages');
     element(by.model('skills.languages_spoken')).sendKeys('languages');
+    element(by.model('agreeCheck')).click();
     element(by.id('stewdentSubmit')).click();
   });
   
@@ -246,6 +296,7 @@ describe('angularjs homepage', function() {
     element(by.model('skills.computer_based')).sendKeys('computer skillz');
     element(by.model('skills.software_skills')).sendKeys('languages');
     element(by.model('skills.languages_spoken')).sendKeys('languages');
+    element(by.model('agreeCheck')).click();
     var clickable = false;
     var submit = element(by.id('stewdentSubmit'));
     expect(submit.isEnabled()).toBe(false);
